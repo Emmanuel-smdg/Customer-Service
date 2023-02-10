4ad2d1cb72bc5219accb44bb7f4966a126cfb080
Emmanuel Samandoulougou <samabdoulougoue@gmail.com>
2023-02-10 09:19:10 +0000

    Updation app properties (don't use Eureka Service)

---
+++ src/main/java/com/emma/customerservice/Entities/CustomerProjection.java
@@ -0,0 +1,10 @@
+package com.emma.customerservice.Entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "fullCustomer", types = Customer.class)
+public interface CustomerProjection extends Projection {
+    public Long getId();
+    public String getName();
+    public String getEmail();
+}
